945162b68b4a9c200d22c2eca79eedc79646290b
Humberto Costa <humberto.tecnologia@gmail.com>
2022-08-26 00:35:28 -0300

    Comentarios para facilitar

--- Codigo/Extensoes/Interface/CtlJanela.asm
+++ Codigo/Extensoes/Interface/CtlJanela.asm
@@ -1,25 +1,32 @@
+
+; Rotina que inicializa um Objeto Controle generico em uma Janela
+
+; Versao usando um texto como titulo
 _interfaceIniciaJanelaRemoto:
     cs call far [Interface.IniciaRemoto]
     cs call far [Interface.AlteraConteudoRemoto]
     jmp _interfaceIniciaJanelaRemotoInterno
 
+; Versao usando um item da traducao como titulo
 _interfaceIniciaJanelaTradRemoto:
     cs call far [Interface.IniciaRemoto]
     cs call far [Interface.AlteraConteudoTradRemoto]
     jmp _interfaceIniciaJanelaRemotoInterno
 
-
+; Rotina principal para criacao do Objeto Controle
 _interfaceIniciaJanelaRemotoInterno:
     mov ax, cs
     es mov [di+ObjControle.PtrRenderiza+2], ax
     es mov word [di+ObjControle.PtrRenderiza], _interfaceJanelaRemoto
     es mov word [di+ObjControle.Tipo], TipoControle.Janela
-    es mov word [di+ObjControle.MargemX1], 2
-    es mov word [di+ObjControle.MargemY1], 16
-    es mov word [di+ObjControle.MargemX2], 2
-    es mov word [di+ObjControle.MargemY2], 2
+    ; Definicao da margem interna da janela em pixels
+    es mov word [di+ObjControle.MargemX1], 2  ; Borda
+    es mov word [di+ObjControle.MargemY1], 16 ; Titulo
+    es mov word [di+ObjControle.MargemX2], 2  ; Borda
+    es mov word [di+ObjControle.MargemY2], 2  ; Borda
     retf
 
+; Rotina que desenha a janela na tela
 _interfaceJanelaRemoto:
     push ax
     push bx
@@ -29,7 +36,7 @@ _interfaceJanelaRemoto:
     push si
     push di
 
-
+    ; Desenha o caixote com borda da janela
     push di
     es mov ax, [di+ObjControle.CalcX1]
     es mov bx, [di+ObjControle.CalcY1]
@@ -39,6 +46,7 @@ _interfaceJanelaRemoto:
     es mov di, [di+ObjControle.CorFundo]
     cs call far [Video.Caixa]
     pop di
+    ; Desenha o caixote da barra de titulos
     push di
     es mov ax, [di+ObjControle.CalcX1]
     add ax, 2
@@ -61,8 +69,10 @@ _interfaceJanelaRemoto:
     es mov ax, [di+ObjControle.CorFundo]
     es mov [di+ObjControle.InternoCor], ax
 
+    ; Carrega o texto do titulo
     call __interfacePtrConteudoLocal
 
+    ; Escreve o titulo na barra de titulo
     .titulo:
         lodsb
         cmp al, 0
--- Codigo/Extensoes/Interface/CtlRotulo.asm
+++ Codigo/Extensoes/Interface/CtlRotulo.asm
@@ -1,15 +1,19 @@
+; Rotina que inicializa um Objeto Controle generico em um Rotulo
 
+; Versao usando um texto como titulo
 _interfaceIniciaRotuloRemoto:
     cs call far [Interface.IniciaRemoto]
     cs call far [Interface.AlteraConteudoRemoto]
     jmp _interfaceIniciaRotuloRemotoInterno
 
+; Versao usando um item da traducao como titulo
 _interfaceIniciaRotuloTradRemoto:
     cs call far [Interface.IniciaRemoto]
     cs call far [Interface.AlteraConteudoTradRemoto]
     jmp _interfaceIniciaRotuloRemotoInterno
 
 
+; Rotina principal para criacao do Objeto Controle
 _interfaceIniciaRotuloRemotoInterno:
     mov ax, cs
     es mov [di+ObjControle.PtrRenderiza+2], ax
@@ -17,6 +21,7 @@ _interfaceIniciaRotuloRemotoInterno:
     es mov word [di+ObjControle.Tipo], TipoControle.Rotulo
     retf
 
+; Rotina que desenha o rotulo na tela
 _interfaceRotuloRemoto:
     push ax
     push bx
@@ -27,6 +32,7 @@ _interfaceRotuloRemoto:
     push di
 
 
+    ; Desenha o fundo do rotulo
     push di
     es mov ax, [di+ObjControle.CalcX1]
     es mov [di+ObjControle.InternoX1], ax
@@ -43,28 +49,44 @@ _interfaceRotuloRemoto:
     es mov di, [di+ObjControle.CorFundo]
     cs call far [Video.Fundo]
     pop di
+    
+    ; Prepara para desenhar o texto
     es mov ax, [di+ObjControle.CorFrente]
     es mov [di+ObjControle.InternoCor], ax
+
+    ; Carrega o texto que esta no conteudo
     call __interfacePtrConteudoLocal
 
+    ; Le o texto, caractere por caractere
     .texto:
+        ; Carrega um caractere do ponteiro
         lodsb
+        ; Verifica se chegou ao fim (Texto terminado em byte 0)
         cmp al, 0
         je .fim
+        ; Verifica se eh um caractere de escape
         cmp al, '\'
         jne .desenha
+            ; Carrega o caractere de escape
             lodsb
+            ; Verifica se chegou ao fim
             cmp al, 0
             je .fim
+            ; Desvia caso seja o escape Nova Linha
             cmp al, 'n'
             je .escapeN
+            ; Se nao for um escape especial escreve este caractere
             jmp .desenha
             .escapeN:
+                ; Escape nova linha
+                ; Volta ao inicio da linha X = X1
                 es mov bx, [di+ObjControle.CalcX1]
                 es mov [di+ObjControle.InternoX1], bx
+                ; Adiciona a altura de uma linha de texto
                 es add word [di+ObjControle.InternoY1], 11
                 jmp .texto
         .desenha:
+            ; Desenha o caractere na tela (Fica no arquivo Interface.asm)
             call __interfaceDesenhaCaractere
         jmp .texto
     .fim:
